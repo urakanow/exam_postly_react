e4d0518830027042e27418436839769fa64c4c85
Yurii Konovalenko <urakanow@gmail.com>
2025-05-30 17:12:39 +0200

    completed contact data block

--- src/components/Create_Offer_Page/CreateOfferPage.js
+++ src/components/Create_Offer_Page/CreateOfferPage.js
@@ -1,9 +1,10 @@
 import CategoryDropdown from "./CategoryDropdown";
-import { useContext, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import Photo from "./Photo";
 import { data, useNavigate } from "react-router";
 import useApi from "../Shared/UseApi";
 import { AuthContext } from "../Shared/AuthContext";
+import ContactDataBlock from "./ContactDataBlock";
 
 function CreateOfferPage() {
     const navigate = useNavigate();
@@ -17,10 +18,12 @@ function CreateOfferPage() {
 
     const [photos, setPhotos] = useState([])
 
-    const [contacter, setContacter] = useState("")
-    const [email, setEmail] = useState("")
-    const [phoneNumber, setPhoneNumber] = useState("")
-    const [address, setAddress] = useState("")
+    const [contactData, setContactData] = useState({
+        contacter: "",
+        email: "",
+        phoneNumber: "",
+        address: "",
+    });
 
     return (
         <div className="create_offer_page_container">
@@ -64,27 +67,13 @@ function CreateOfferPage() {
                 </div>
             </div>
 
-            <div className="create_offer_page_section">
-                <h1 className="section_heading">Контактні дані</h1>
-
-                <label className="text_input_label" htmlFor="contacter">Контактна особа</label>
-                <input type="text" className="form_text_input text_input" id="contacter" onChange={(e) => setContacter(e.target.value)}/>
-                
-                <label className="text_input_label" htmlFor="email">Ел. Пошта</label>
-                <input type="text" className="form_text_input text_input" id="email" onChange={(e) => setEmail(e.target.value)}/>
-
-                <label className="text_input_label" htmlFor="phone_number">Номер телефону</label>
-                <input type="text" className="form_text_input text_input" id="phone_number" onChange={(e) => setPhoneNumber(e.target.value)}/>
-
-                <label className="text_input_label" htmlFor="address">Місце знаходження</label>
-                <input type="text" className="form_text_input text_input" id="address" onChange={(e) => setAddress(e.target.value)}/>
-            </div>
+            <ContactDataBlock formData={contactData} setFormData={setContactData} />
 
             <button className="green_button" id="add_offer_button" onClick={createOffer}>Додати Оголошення</button>
         </div>
      );
 
-     async function createOffer(){
+    async function createOffer(){
         try {
             const formData = new FormData();
                 
@@ -92,15 +81,15 @@ function CreateOfferPage() {
             formData.append('description', description);
             formData.append('category', category);
             formData.append('price', price);
-            formData.append('contacter', contacter);
-            formData.append('email', email);
-            formData.append('phoneNumber', phoneNumber);
-            formData.append('address', address);
             
             photos.forEach((photo, index) => {
                 formData.append(`Images`, photo); // Key can be just "photos" for multiple files
             });
-
+            
+            Object.entries(contactData).forEach(([key, value]) => {
+                formData.append(key, value);
+            });
+            
             console.log(formData);
 
             const response = await authorizedRequest({
