ff8acff7fe14748c71b1409fd72d6926499a959c
Yurii Konovalenko <urakanow@gmail.com>
2025-05-31 12:53:22 +0200

    finished

---
+++ src/components/Favorites_Page/FavoritesPage.js
@@ -0,0 +1,60 @@
+import { useContext, useEffect, useState } from "react";
+import useApi from "../Shared/UseApi";
+import { AuthContext } from "../Shared/AuthContext";
+import { Grid } from "@mui/material";
+import OfferElement from "../Main_Page/OfferElement";
+
+function FavoritesPage() {
+    const { baseUrl } = useContext(AuthContext);
+    const [favorites, setFavorites] = useState(null);
+    const { authorizedRequest } = useApi()
+
+    useEffect(() => {
+        fetchFavorites();
+    }, [])
+
+    useEffect(() => {
+        console.log(favorites);
+    }, [favorites])
+
+    return (
+        <div id="favorites_page_container">
+            <h1 className="large_heading">Вибрані</h1>
+            {favorites ? (
+                <div className="offers_block">
+                    <Grid container spacing={2} className="offers_grid">
+                        {favorites.map((offer, index) =>
+                            <OfferElement key={index} offerData={offer} linkUrl={"offer"} onFavoriteClick={(id) => {
+                                console.log("remove favorite ", id);
+                                setFavorites(prev => prev.filter(offer => offer.id !== id));
+                            }}/>
+                        )}
+
+                    </Grid>
+                    
+                </div>
+            ) : (
+                <>
+                    loading...
+                </>
+            )}
+        </div>
+     );
+
+     async function fetchFavorites() {
+        try{
+            const response = await authorizedRequest({
+                method: 'get',
+                url: `${baseUrl}/favorite/get-user-favorites`
+            })
+
+            if(response.status === 200){
+                setFavorites(response.data)
+            }
+        } catch(err) {
+            console.error("failed to fetch favorites: ", err)
+        }
+     }
+}
+
+export default FavoritesPage;
